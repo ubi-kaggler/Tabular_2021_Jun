b97789d599298b749de24b1cac0c8e8c57645387
Hiroki-Ishimaru <maru.gooooooo@gmail.com>
2021-06-13 23:45:16 +0900

    under+bagging

--- ishimaru/eda_ishimaru.py
+++ ishimaru/eda_ishimaru.py
@@ -1,9 +1,71 @@
+import numpy as np
 import pandas as pd
-from pandas_profiling import ProfileReport
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# matplotlib setting
+mpl.rcParams['figure.dpi'] = 200
+mpl.rcParams['axes.spines.top'] = False
+mpl.rcParams['axes.spines.right'] = False
 
 train = pd.read_csv('../data/train.csv')
 test = pd.read_csv('../data/test.csv')
+sample_submission = pd.read_csv('../data/sample_submission.csv')
+
+train = train.drop('id', axis=1)
+test = test.drop('id', axis=1)
+
+# # 対象分布
+# fig = plt.figure(figsize=(12, 8))
+# gs = fig.add_gridspec(7, 4)
+# ax = fig.add_subplot(gs[:-1,:])
+# ax2 = fig.add_subplot(gs[-1,:])
+# ax2.axis('off')
+#
+target_cnt = train['target'].value_counts().sort_index()
+target_cum = target_cnt.cumsum()
+# ax.bar(target_cnt.index, target_cnt, color=['#d4dddd' if i%2==0 else '#fafafa' for i in range(9)],
+#        width=0.55,
+#        edgecolor='black',
+#        linewidth=0.7)
+#
+#
+# for i in range(9):
+#     ax.annotate(f'{target_cnt[i]}({target_cnt[i]/len(train)*100:.3}%)', xy=(i, target_cnt[i]+1000),
+#                    va='center', ha='center',
+#                )
+#     ax2.barh([0], [target_cnt[i]], left=[target_cum[i] - target_cnt[i]], height=0.2,
+#             edgecolor='black', linewidth=0.7, color='#d4dddd' if i%2==0 else '#fafafa'
+#             )
+#     ax2.annotate(i+1, xy=(target_cum[i]-target_cnt[i]/2, 0),
+#                  va='center', ha='center', fontsize=10)
+#
+# ax.set_title('対象分布', weight='bold', fontsize=15)
+# ax.grid(axis='y', linestyle='-', alpha=0.4)
+#
+# fig.tight_layout()
+# plt.show()
+
+target_cnt_df = pd.DataFrame(target_cnt)
+target_cnt_df['ratio(%)'] = target_cnt_df/target_cnt.sum()*100
+target_cnt_df.sort_values('ratio(%)', ascending=False, inplace=True)
+target_cnt_df['cummulated_sum(%)'] = target_cnt_df['ratio(%)'].cumsum()
+
+fig, ax = plt.subplots(1, 1, figsize=(15, 6))
+
+ax.bar(range(75), 100, linewidth=0.2, edgecolor='black', alpha=0.2, color='lightgray')
+#ax.bar(range(75), ((train == 0).sum() / len(train)*100)[:-1].sort_values(), linewidth=0.2, edgecolor='black', alpha=1, color='#244747')
+ax.bar(range(75), ((train == 0).sum() / len(train)*100)[:-1], linewidth=0.2, edgecolor='black', alpha=1, color='#244747')
 
 
-# profile = ProfileReport(train, title="Pandas Profiling Report")
-# profile.to_file("profile_report.html") # HTML ファイルを保存する
+ax.set_ylim(0, 100)
+ax.set_yticks(range(0, 100, 10))
+
+ax.set_xticks(range(0, 75, 5))
+ax.margins(0.01)
+ax.grid(axis='y', linestyle='--', linewidth=0.2, zorder=5)
+ax.set_title('Ratio of Zeros (Sorted)', loc='center', fontweight='bold')
+ax.set_ylabel('ratio(%)', fontsize=12)
+ax.legend()
+plt.show()
--- ishimaru/light.py
+++ ishimaru/light.py
@@ -1,50 +1,82 @@
 import pandas as pd
+import numpy as np
 
 train = pd.read_csv('../data/train.csv')
 test = pd.read_csv('../data/test.csv')
 train = train.drop(columns=['id'])
 test = test.drop(columns=['id'])
+print(train.info())
 
 train_x = train.drop(['target'], axis=1)
 train_y = train['target']
 
 test_x = test.copy()
 
-from sklearn import preprocessing
-le = preprocessing.LabelEncoder()
-le.fit(train_y)
-train_y = le.transform(train_y)
-train_y = pd.Series(train_y)
 
-import lightgbm as lgb
-from sklearn.metrics import log_loss
+# Under Samplingの関数（X: num:ターゲット件数 label:少数派のラベル）
 
-from sklearn.model_selection import train_test_split
-tr_x, va_x, tr_y, va_y = train_test_split(train_x, train_y, test_size=0.2, shuffle=True, random_state=42, stratify=train_y)
+def under_sampling_func(X, num, label):
+    # KMeansによるクラスタリング
+    from sklearn.cluster import KMeans
+    km = KMeans(random_state=201707)
+    km.fit(X, train_y)
+    X['Cluster'] = km.predict(X)
 
-lgb_train = lgb.Dataset(tr_x, tr_y)
-lgb_eval = lgb.Dataset(va_x, va_y)
+    # 群別の構成比を少数派の件数に乗じて群別の抽出件数を計算
+    count_sum = X.groupby('Cluster').count().iloc[0:,0].as_matrix()
+    ratio = count_sum / count_sum.sum()
+    samp_num = np.round(ratio * num, 0).astype(np.int32)
 
-params = {'objective': 'multiclass',
-                  'random_state': 10,
-                  'metric': 'multi_logloss',
-             'num_class': 9}
+    # 群別にサンプリング処理を実施
+    for i in np.arange(8):
+        tmp = X[X['Cluster'] == i]
+        if i == 0:
+            tmp1 = X.sample(samp_num[i], replace=True)
+        else:
+            tmp2 = X.sample(samp_num[i], replace=True)
+            tmp1 = pd.concat([tmp1, tmp2])
+    tmp1['Class'] = label
+    return tmp1
 
-model = lgb.train(params,
-            lgb_train,
-            num_boost_round=50,
-            valid_names=['train', 'valid'], valid_sets=[lgb_train, lgb_eval])
 
-va_pred = model.predict(va_x)
-score = log_loss(va_y, va_pred)
-print(f'logloss: {score:.4f}')
+under_sampling_func(train_x, 200000, 'Class_5')
 
-pred = model.predict(test_x)
-print(len(pred))
 
-submission = pd.DataFrame({'id': list(range(200000,300001))})
-pred = pd.DataFrame(pred,columns =["Class_1","Class_2","Class_3","Class_4","Class_5","Class_6","Class_7","Class_8","Class_9"])
-submission = pd.merge(submission,pred,left_index=True, right_index=True)
-submission.to_csv('./sample_submission.csv', index=False)
-print('csv output')
+# from sklearn import preprocessing
+# le = preprocessing.LabelEncoder()
+# le.fit(train_y)
+# train_y = le.transform(train_y)
+# train_y = pd.Series(train_y)
+#
+# import lightgbm as lgb
+# from sklearn.metrics import log_loss
+#
+# from sklearn.model_selection import train_test_split
+# tr_x, va_x, tr_y, va_y = train_test_split(train_x, train_y, test_size=0.2, shuffle=True, random_state=42, stratify=train_y)
+#
+# lgb_train = lgb.Dataset(tr_x, tr_y)
+# lgb_eval = lgb.Dataset(va_x, va_y)
+#
+# params = {'objective': 'multiclass',
+#                   'random_state': 10,
+#                   'metric': 'multi_logloss',
+#              'num_class': 9}
+#
+# model = lgb.train(params,
+#             lgb_train,
+#             num_boost_round=50,
+#             valid_names=['train', 'valid'], valid_sets=[lgb_train, lgb_eval])
+#
+# va_pred = model.predict(va_x)
+# score = log_loss(va_y, va_pred)
+# print(f'logloss: {score:.4f}')
+#
+# pred = model.predict(test_x)
+# print(len(pred))
+#
+# submission = pd.DataFrame({'id': list(range(200000,300001))})
+# pred = pd.DataFrame(pred,columns =["Class_1","Class_2","Class_3","Class_4","Class_5","Class_6","Class_7","Class_8","Class_9"])
+# submission = pd.merge(submission,pred,left_index=True, right_index=True)
+# submission.to_csv('./sample_submission.csv', index=False)
+# print('csv output')
 
